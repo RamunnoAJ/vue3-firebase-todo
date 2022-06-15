95e702211b3be6691825b55298cb2f453680ed68
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-15 14:47:43 -0300

    adding firestore to the project

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBzeW_Af-B9i79OIYidya5emF9vdJdmRsI',
@@ -10,3 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
