222c2feb5627e46a4364f919ae494fa2d6f29d80
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-15 14:40:33 -0300

    setting up firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzeW_Af-B9i79OIYidya5emF9vdJdmRsI',
+  authDomain: 'vue3-my-todo.firebaseapp.com',
+  projectId: 'vue3-my-todo',
+  storageBucket: 'vue3-my-todo.appspot.com',
+  messagingSenderId: '558662530417',
+  appId: '1:558662530417:web:05c25b0b35f5e42aea32d0',
+};
+
+const app = initializeApp(firebaseConfig);
