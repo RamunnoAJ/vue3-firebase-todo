74de3b1ee43e20c8b1d2445b454a1ed76f102764
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-15 15:37:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-firebase-todo',
+  base: '/vue3-firebase-todo/',
   plugins: [vue()],
   resolve: {
     alias: {
